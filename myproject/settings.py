7c09d4da61fca69e4db091a256f164edb781cb63
vyshnavcp <vyshnavcp01@gmail.com>
2026-02-19 18:59:09 +0530

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -52,6 +52,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'myapp.context_processors.cart_count',
+                
             ],
         },
     },
